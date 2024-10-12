fcd0f3fe7068117ed3d82a4fec6a14f7b9893882
argoniaf <observerhelen666@gmail.com>
2024-10-12 22:00:02 +0900

    blaack5

--- src/main.c
+++ src/main.c
@@ -1,5 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <time.h>
+#include <ncurses.h>
 #include "tetris.h"
 
 TetBlock tet_templates[] = {
@@ -48,26 +50,41 @@ void printTetGame(TetGame *tetg)
     {
         for (int j = 0; j < tf->width; j++)
         {
-            int sym = 0;
+            int sym = 1;
             if (tf->blocks[i * tf->width + j].b != 0)
-                sym = 1;
+                sym = 2;
             else
             {
                 int x = j - t->x;
                 int y = i - t->y;
                 if (x >= 0 && x < t->size && y >= 0 && y < t->size)
                     if (t->blocks[y * t->size + x].b != 0)
-                        sym = 1;
+                        sym = 2;
             }
-            printf("%d", sym);
+            attron(COLOR_PAIR(sym));
+            mvaddch(i,j,' ');
+            attroff(COLOR_PAIR(sym));
         }
     }
-    fflush(stdout);
 }
 
 int main(int argc, char *argv[])
 {
+    struct timespec sp_start, sp_end, ts1, ts2 = {0,0};
+
+    initscr();
+    start_color();
+    init_pair(1, COLOR_WHITE, COLOR_MAGENTA);
+    init_pair(2, COLOR_GREEN, COLOR_GREEN);
+    cbreak(); //влияет на строковый буфер и принуждает к немедленному выводу символа нажатой клавиши
+    noecho(); //отлкючает вывод символа нажатой клавиши на экран
+    nodelay(stdscr, TRUE); //отключает задержку при вызове функции гетч
+    scrollok(stdscr, TRUE); //отключает возможность скроллинга
+
+
     TetGame *tetg = createTetGame(34, 30, 5, 6, tet_templates);
+
+
     TetPlayer player;
     player.action = TET_PLAYER_NOP;
     tetg->player = &player;
@@ -75,11 +92,44 @@ int main(int argc, char *argv[])
 
     while (tetg->playing != TET_GAMEOVER)
     {
+        clock_gettime(CLOCK_MONOTONIC, &sp_start);
+
+        int ch = getch();
+        switch(ch){
+            case 'w':
+              player.action = TET_PLAYER_UP;
+              break;
+            case 's':
+              player.action = TET_PLAYER_DOWN;
+              break;
+            case 'a':
+              player.action = TET_PLAYER_LEFT;
+              break;
+            case 'd':
+              player.action = TET_PLAYER_RIGHT;
+              break;
+            default:
+              player.action = TET_PLAYER_NOP;
+              break;
+        }
+        
         calculateTet(tetg);
         printTetGame(tetg);
+
+        attron(COLOR_PAIR(1));
+        mvprintw(0,0, "Score: %d", tetg->score);
+        move(tetg->field->height+1, tetg->field->width+1);
+        attroff(COLOR_PAIR(1));
+    //процедура вывода не отображает строку сразу, а только подготавливает для вывода
+    //чтобы вывести текст на экране используем процедуру refresh
+        refresh();
+        clock_gettime(CLOCK_MONOTONIC, &sp_end);
+        if(sp_end.tv_sec - sp_start.tv_sec <= 0 &&
+        (ts2.tv_sec = 33000000 - (sp_end.tv_nsec -sp_start.tv_nsec))>0)
+        nanosleep(&ts2, &ts1);
     }
 
     freeTetGame(tetg);
-    printf("Tetris!\n\n");
+    endwin(); //метод,чтобы вернуть исходний вид окна терминала и не нарушить ее внутреннюю структуру
     return 0;
 };
